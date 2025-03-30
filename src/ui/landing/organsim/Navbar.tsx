da5d0270d0abf17b4816fb005d3019e4324596bf
aaryae <aaryaed@gmail.com>
2025-03-30 21:01:11 +0545

    feat: navbar, sidebar and topbar completed

--- src/ui/landing/organsim/Navbar.tsx
+++ src/ui/landing/organsim/Navbar.tsx
@@ -6,18 +6,21 @@ import {
   Briefcase,
   Mail,
   ShoppingCart,
+  X,
 } from "lucide-react";
 import { useState } from "react";
 
 const Navbar = () => {
   const [navstate, setNavstate] = useState(false);
+  const [messagestate, setMessagestate] = useState(false);
+  const [profilestate, setProfilestate] = useState(false);
 
   return (
     <>
       <div className="flex">
         {/* First Sidebar */}
         <div
-          className="h-screen fixed w-fit p-7 bg-[#262626] text-white hidden md:block  border-r border-[#ffffff27] z-10"
+          className="h-screen fixed w-fit px-7 py-6 bg-[#262626] text-white hidden md:block  border-r border-[#ffffff27] z-10"
           onClick={() => {
             setNavstate(!navstate);
           }}
@@ -90,8 +93,88 @@ const Navbar = () => {
           <AlignLeft color="#ffffff" strokeWidth={0.5} absoluteStrokeWidth />
         </div>
         <div className="flex gap-3  px-5">
-          <Mail color="#ffffff" strokeWidth={0.5} absoluteStrokeWidth />
-          <div className="p-3 rounded-full bg-[#188502]"></div>
+          <Mail
+            className="relative cursor-pointer"
+            color="#ffffff"
+            strokeWidth={0.5}
+            absoluteStrokeWidth
+            onClick={() => {
+              setMessagestate(!messagestate);
+              setProfilestate(false);
+            }}
+          />
+          <div
+            className={`absolute  bg-[#171717] border-1 border-[#ffffff1c] rounded-xl h-64 p-5 text-[#ffffffb2] flex flex-col justify-between right-16 top-11 transition-all ease-in-out ${messagestate ? "flex" : "hidden"} `}
+          >
+            <div className="flex justify-between p-2 border-b-1 border-[#ffffffb2]">
+              <h1 className="text-2xl">Message</h1>
+              <X
+                color="#880808"
+                onClick={() => {
+                  setMessagestate(!messagestate);
+                  setProfilestate(false);
+                }}
+              />
+            </div>
+
+            <h3 className="text-md text-[#ffffffb2]">
+              You don't have message at the moment
+            </h3>
+
+            <div className="w-full text-center text-blue-800 cursor-pointer hover:underline">
+              View all
+            </div>
+          </div>
+          <div
+            className="p-3 rounded-full bg-[#188502] relative cursor-pointer"
+            onClick={() => {
+              setProfilestate(!profilestate);
+              setMessagestate(false);
+            }}
+          ></div>
+          <div
+            className={`absolute  bg-[#171717] border-1 border-[#ffffff1c] rounded-xl w-48 p-5 text-[#ffffffb2] flex-col right-10 top-11 transition-all ease-in-out ${profilestate ? "flex" : "hidden"} `}
+          >
+            <div className="flex  justify-between py-2 border-b-1 border-[#ffffffb2]">
+              <div className="text-md tracking-wide ">Your Profile</div>
+              <X
+                color="#880808"
+                onClick={() => {
+                  setProfilestate(!profilestate);
+                  setMessagestate(false);
+                }}
+              />
+            </div>
+
+            <div className="text-md text-[#ffffffb2]">
+              <nav className="flex flex-col gap-3 mt-3">
+                <a
+                  href="#"
+                  className="flex items-center space-x-2 text-gray-300 hover:text-white"
+                >
+                  <ShoppingCart size={20} /> <span>Ecommerce</span>
+                </a>
+                <a
+                  href="#"
+                  className="flex items-center space-x-2 text-gray-300 hover:text-white"
+                >
+                  <Briefcase size={20} /> <span>Project</span>
+                </a>
+                <a
+                  href="#"
+                  className="flex items-center space-x-2 text-gray-300 hover:text-white"
+                >
+                  <BarChart2 size={20} /> <span>Marketing</span>
+                </a>
+                <a
+                  href="#"
+                  className="flex items-center space-x-2 text-gray-300 hover:text-white"
+                >
+                  <Activity size={20} /> <span>Analytic</span>
+                </a>
+              </nav>
+            </div>
+          </div>
         </div>
       </div>
     </>
