e748d8c0c503c4e77cf5487463502b33de4ef8ec
Ankit <ankit001nep@gmail.com>
2025-05-12 12:14:54 +0545

    fix: date in footer

--- src/ui/landing/organsim/Footer.tsx
+++ src/ui/landing/organsim/Footer.tsx
@@ -1,11 +1,13 @@
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="bg-[#262626] p-2 mt-6 text-center text-white">
       <div className="max-w-6xl mx-auto px-6 flex flex-col md:flex-row justify-between items-center gap-10">
         <div className=" flex justify-between w-full text-center md:text-left">
           <h2 className="text-2xl font-light">Srijan</h2>
           <p className="text-md mt-4">
-            © 2035 by Srijan and secured by also srijan
+            © {currentYear} by Srijan. All rights reserved
           </p>
         </div>
       </div>
