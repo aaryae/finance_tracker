2318d512c70ee0875c9d059d23f49063feeed581
Ankit <ankit001nep@gmail.com>
2025-05-14 00:39:50 +0545

    add:Logout in UserPage

--- src/ui/landing/organsim/navigationbars/Topbar.tsx
+++ src/ui/landing/organsim/navigationbars/Topbar.tsx
@@ -1,13 +1,10 @@
 import {
-  Activity,
   AlignLeft,
-  BarChart2,
-  Briefcase,
-  Mail,
-  ShoppingCart,
-  X,
+  LogOut,
+  X
 } from "lucide-react";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Topbar = ({
   setNavstate,
@@ -16,6 +13,17 @@ const Topbar = ({
 }) => {
   const [messagestate, setMessagestate] = useState(false);
   const [profilestate, setProfilestate] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("userId");
+    localStorage.removeItem("refreshToken");
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("isLoggedIn");
+    // Optionally, navigate to the login page
+    navigate("/login");
+  };
 
   return (
     <>
@@ -28,20 +36,10 @@ const Topbar = ({
         </div>
 
         <div className="flex gap-3 px-5">
-          <Mail
-            className="relative cursor-pointer"
-            color="#ffffff"
-            strokeWidth={0.5}
-            absoluteStrokeWidth
-            onClick={() => {
-              setMessagestate(!messagestate);
-              setProfilestate(false);
-            }}
-          />
+
           <div
-            className={`absolute bg-[#171717] border-1 border-[#ffffff1c] rounded-xl h-64 p-5 text-[#ffffffb2] flex-col justify-between right-16 top-11 transition-all ease-in-out ${
-              messagestate ? "flex" : "hidden"
-            }`}
+            className={`absolute bg-[#171717] border-1 border-[#ffffff1c] rounded-xl h-64 p-5 text-[#ffffffb2] flex-col justify-between right-16 top-11 transition-all ease-in-out ${messagestate ? "flex" : "hidden"
+              }`}
           >
             <div className="flex justify-between p-2 border-b-1 border-[#ffffffb2]">
               <h1 className="text-2xl">Message</h1>
@@ -63,9 +61,8 @@ const Topbar = ({
             }}
           ></div>
           <div
-            className={`absolute bg-[#171717] border-1 border-[#ffffff1c] rounded-xl w-48 p-5 text-[#ffffffb2] flex-col right-10 top-11 transition-all ease-in-out ${
-              profilestate ? "flex" : "hidden"
-            }`}
+            className={`absolute bg-[#171717] border-1 border-[#ffffff1c] rounded-xl w-48 p-5 text-[#ffffffb2] flex-col right-10 top-11 transition-all ease-in-out ${profilestate ? "flex" : "hidden"
+              }`}
           >
             <div className="flex justify-between py-2 border-b-1 border-[#ffffffb2]">
               <div className="text-md tracking-wide">Your Profile</div>
@@ -74,30 +71,15 @@ const Topbar = ({
 
             <div className="text-md text-[#ffffffb2]">
               <nav className="flex flex-col gap-3 mt-3">
-                <a
-                  href="#"
-                  className="flex items-center space-x-2 text-gray-300 hover:text-white"
+                <button
+                  onClick={() => {
+
+                    handleLogout();
+                  }}
+                  className={`flex items-center space-x-2 hover:text-white`}
                 >
-                  <ShoppingCart size={20} /> <span>Ecommerce</span>
-                </a>
-                <a
-                  href="#"
-                  className="flex items-center space-x-2 text-gray-300 hover:text-white"
-                >
-                  <Briefcase size={20} /> <span>Project</span>
-                </a>
-                <a
-                  href="#"
-                  className="flex items-center space-x-2 text-gray-300 hover:text-white"
-                >
-                  <BarChart2 size={20} /> <span>Marketing</span>
-                </a>
-                <a
-                  href="#"
-                  className="flex items-center space-x-2 text-gray-300 hover:text-white"
-                >
-                  <Activity size={20} /> <span>Analytic</span>
-                </a>
+                  <LogOut size={20} /> <span>Logout</span>
+                </button>
               </nav>
             </div>
           </div>
